ec9cf9826fe519247d8c5ed9dd64fb834f39cb2f
Janexmgd <janexmgd@gmail.com>
2022-03-18 19:06:49 +0700

    menambah validasi type data

--- nomor1.js
+++ nomor1.js
@@ -1,22 +1,30 @@
 //nomor1 
 
 //its Palindrom?
-// const cekPalindrom = (kata) => {
-//     caseKata = kata.toLowerCase()
-//     let text = caseKata.split('').reverse().join('')
-//     if (text == caseKata) {
-//         console.log("Palindrom");
-//     } else {
-//         console.log("Bukan Palindrom");
-//     }
-// }
+const cekPalindrom = (kata) => {
+    if (typeof (kata) == "string") {
+        caseKata = kata.toLowerCase()
+        let text = caseKata.split('').reverse().join('')
+        if (text == caseKata) {
+            console.log("Palindrom");
+        } else {
+            console.log("Bukan Palindrom");
+        }
+    } else {
+        console.log("Type data harus berupa string");
+    }
+}
 
-// cekPalindrom("Viola")
+// cekPalindrom("saya")
 // cekPalindrom("MaLAM")
 
 //ReverseWord
 const ReverseWord = (kalimat) => {
-    const balik = kalimat.split(' ').reverse('').join(' ')
-    console.log(balik);
+    if (typeof (kalimat) == "string") {
+        const balik = kalimat.split(' ').reverse('').join(' ')
+        console.log(balik);
+    } else {
+        console.log("Parameter kalimat harus bertype data string");
+    }
 }
-ReverseWord("Saya belajar Javascript")
+ReverseWord("Saya udah makan")
--- nomor3.js
+++ nomor3.js
@@ -1,15 +1,20 @@
 //no3
 function divideAndSort(num) {
     if (typeof (num) == "number") {
-        let hasil = '';
-        let numArray = num.toString().split('0');
-        numArray.forEach(element => {
-            hasil = hasil + element.split('').sort().join('')
-        });
-        console.log(hasil);
+        if (num < Number.MAX_SAFE_INTEGER) {
+            let hasil = '';
+            let numArray = num.toString().split('0');
+            numArray.forEach(element => {
+                hasil = hasil + element.split('').sort().join('')
+            });
+            console.log(hasil);
+        } else {
+            console.log("Tidak menampilkan hasil karena melebihi max_safe_integer");
+        }
     } else {
         console.log("parameter harus number");
     }
 }
-divideAndSort(74108520159098987);
+divideAndSort(9007199254740991)
+divideAndSort(74108520159098987741085201590989877410852015909898774108520159098987);
 divideAndSort(5956560159466056)
